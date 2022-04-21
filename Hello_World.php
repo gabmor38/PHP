f9c843494ebddcbd9a152bce00e56bf4702df519
gabmor38 <Gabriela.morenor@gmail.com>
2022-04-20 21:44:31 -0400

    connected to XAMPP

--- Hello_World.php
+++ Hello_World.php
@@ -1,6 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$greeting = "Hello World";
-echo $greeting;
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP_Exercises</title>
+</head>
 
-?>
+<body>
+
+  <?php
+
+  $greeting = "Hello World";
+  echo $greeting;
+
+  ?>
+
+</body>
+
+</html>
